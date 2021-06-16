78d41d18cbe78ba157a523ae563bee0b4371d031
kittidaj <kittidaj@hotmail.com>
2021-06-16 11:23:19 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 11:21:49 2021
+
+@author: KIT
+"""
+
+import shutil
+import os
+import sys
+
+
+#A = 0
+#B = 0
+#SUM = 0
+
+def Test():
+    #Insert code of Execute Test here
+    print("Test file is running")
+
+
+if __name__ == "__main__":
+    #Call function here
+    Test()
